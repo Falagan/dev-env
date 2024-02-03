7417a63dd14e19492b5ff001ff943ce10de8efd7
Eloy Perez <ep.falagan@gmail.com>
2024-02-03 16:37:12 -0500

    setup ssh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
 sudo sudo apt install git-all
-sudo mkdir ~/Personal
-sudo cd ~/Personal
-sudo git clone https://github.com/Falagan/dev-env.git
-sudo cd dev-env
-sudo ansible-playbook --ask-vault-pass local.yml
+#mkdir ~/Personal
+#cd ~/Personal
+#git clone https://github.com/Falagan/dev-env.git
+#cd dev-env
+ansible-playbook --ask-vault-pass local.yml
