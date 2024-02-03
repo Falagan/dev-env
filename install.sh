47970204b8098955d79b6144bef8a892997e2c98
Eloy Perez <ep.falagan@gmail.com>
2024-02-03 16:44:35 -0500

    remove debug path in task ssh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
 sudo sudo apt install git-all
-#mkdir ~/Personal
-#cd ~/Personal
-#git clone https://github.com/Falagan/dev-env.git
-#cd dev-env
+mkdir ~/Personal
+cd ~/Personal
+git clone https://github.com/Falagan/dev-env.git
+cd dev-env
 ansible-playbook --ask-vault-pass local.yml
