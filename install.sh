10ab8aad8fe3df53580674e7f9c209cac67d6a2c
Eloy Perez <ep.falagan@gmail.com>
2024-05-27 12:49:15 +0200

    feat: Add Discord installation task

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
 sudo sudo apt install git-all
-mkdir ~/Personal
-mkdir ~/Personal/Projects
-cd ~/Personal
+mkdir ~/personal
+mkdir ~/personal/projects
+cd ~/personal
 git clone https://github.com/Falagan/dev-env.git
 cd dev-env
 ansible-playbook --ask-vault-pass local.yml
